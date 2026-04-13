66154da0d89dbbd2d03720a4920fa64b1de1ddee
Ilya Degtyarik <degtyarik.ilya@gmail.com>
2026-04-14 02:33:28 +0300

    fixes

--- apps/mobile/android/app/src/main/java/com/tggf/app/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/tggf/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -13,15 +14,30 @@ import com.tggf.app.localapi.LocalApiBridgePlugin;
 
 public class MainActivity extends BridgeActivity {
     private static final int NOTIFICATION_PERMISSION_REQUEST_CODE = 7002;
+    private static final String TAG = "tg-gf/MainActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Enable WebView remote debugging for easier diagnostics via chrome://inspect.
-        WebView.setWebContentsDebuggingEnabled(true);
         registerPlugin(LocalApiBridgePlugin.class);
         super.onCreate(savedInstanceState);
+        enableWebViewDebugging("onCreate");
         requestNotificationPermissionIfNeeded();
         ForegroundSyncService.ensureStartedIfEnabled(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Some devices/WebView updates can reset this flag; enforce it again.
+        enableWebViewDebugging("onResume");
+    }
+
+    private void enableWebViewDebugging(String reason) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+        WebView.setWebContentsDebuggingEnabled(true);
+        Log.i(TAG, "WebView debugging enabled (" + reason + ")");
     }
 
     private void requestNotificationPermissionIfNeeded() {
